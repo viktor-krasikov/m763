dfb92502f49c1305bd10526d1ac0c3dc726dfc5d
Чимит Гармаев <iheky87@gmail.com>
2023-10-10 15:44:28 +0800

    Выполнил доп задание, сделал строку и столбец для матрицы

--- lesson3/tasks.py
+++ lesson3/tasks.py
@@ -9,28 +9,30 @@ matr = [
 
 def garmaev_glavinskaya_tumene(matr):
     maximum = 0
-    for i in range(len(matr)):
-        for j in range(len(matr[i])):
+    stroka = len(matr)
+    stolb = len(matr[0])
+    for i in range(stroka):
+        for j in range(stolb):
             if matr[i][j] > maximum:
                 maximum = matr[i][j]
                 x = i
                 y = j
-
-
-    if x == len(matr)-1 or (x+1 < len(matr) and matr[x+1][y] == -1):
+    if x == stroka-1 or (x+1 < stroka and (matr[x+1][y] == -1 or matr[x+1][y] > 1)):
+        for i in range(stroka):
+            for j in range(stolb):
         for i in range(len(matr)):
             for j in range(len(matr[i])):
                 if matr[i][j] > 0:
                     matr[i][j] = 0
-
-
-    elif x+1 < len(matr) and matr[x+1][y] == 0:
+    elif x+1 < stroka and matr[x+1][y] == 0:
+        for i in range(stroka):
+            for j in range(stolb):
         for i in range(len(matr)):
             for j in range(len(matr[i])):
                 if matr[i][j] > 0:
                     matr[i][j] = matr[i][j] - 1
         matr[x+1][y] = maximum
-
+    elif x+1 < stroka and matr[x+1][y] == -2:
     elif x+1 < len(matr) and matr[x+1][y] == -2:
         matr[x+1][y] = maximum + 1
 
