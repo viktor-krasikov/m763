e786249937940d1adca4af8a612b5fee59f8eb5f
L4zaru7 <cake.maks@mail.ru>
2023-10-19 17:38:04 +0800

    Changed snake Mordvin Nikolaeva

--- lesson3/snake_nikolaeva_mordvin.py
+++ lesson3/snake_nikolaeva_mordvin.py
@@ -1,7 +1,7 @@
+import pygame
+import numpy as np
 import time
 import tasks
-import numpy as np
-import pygame
 
 pygame.init()
 board = np.zeros((30, 40))
@@ -10,7 +10,7 @@ disp = pygame.display.set_mode((400, 300))
 
 def get_color(value):
     if value == -2:
-        return (0, 128, 0)
+        return (180, 0, 0)
     elif value > 0:
         return (200, 200, 0)
     elif value == -1:
@@ -24,39 +24,44 @@ def draw_board():
         for j, elem in enumerate(row):
             if elem != 0:
                 pygame.draw.rect(disp, get_color(elem), [j * 10, i * 10, 10, 10])
+
     pygame.display.update()
 
 
-game_over = False
 direction = 'R'
+game_over = False
 tasks.create_snake_if_need(board)
 tasks.create_food_if_need(board)
 while not game_over:
+    tasks.create_snake_if_need(board)
+    tasks.create_food_if_need(board)
     draw_board()
+
+    # Обработка событий
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
-        if event.type == pygame.KEYDOWN:
+        if event.type == pygame.KEYDOWN:  # Добавить проверку на то, можно ли поворачивать
             if event.key == pygame.K_LEFT:
                 direction = 'L'
-            elif event.key == pygame.K_UP:
+            if event.key == pygame.K_UP:
                 direction = 'U'
-            elif event.key == pygame.K_RIGHT:
-                direction = 'R'
-            elif event.key == pygame.K_DOWN:
+            if event.key == pygame.K_DOWN:
                 direction = 'D'
-            elif event.key == pygame.K_ESCAPE:
-                pygame.quit()
-                quit()
+            if event.key == pygame.K_RIGHT:
+                direction = 'R'
     if direction == 'R':
-        board = tasks.lavrentev_buldaev(board)
-    elif direction == 'D':
+        board = tasks.step_right(board)
+    if direction == 'D':
         tasks.step_down(board)
-    elif direction == 'L':
+    if direction == 'L':
         tasks.step_left(board)
-    elif direction == 'U':
+    if direction == 'U':
         tasks.step_up(board)
 
+    draw_board()
+
     time.sleep(0.1)
+
 pygame.quit()
 quit()
