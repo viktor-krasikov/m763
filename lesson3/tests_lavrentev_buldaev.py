d887c1009e5e2e6da00a07b66f1b97fb990cc1fe
Lavres03 <st.la.2001@mail.ru>
2023-10-19 17:48:54 +0800

    tests

--- lesson3/tests_lavrentev_buldaev.py
+++ lesson3/tests_lavrentev_buldaev.py
@@ -1,89 +1,90 @@
 import unittest
 import tasks
+import numpy as np
 
 
 class CheckMatrixTestCase(unittest.TestCase):
     def test_1t(self):
-        a = [[1, -12, 0],
-             [4, 6, 20],
-             [-1, 10, -1]]
-        res = tasks.lavrentev_buldaev(a)
-        expected = [[0, -12, 0],
-                    [0, 0, 0],
-                    [-1, 0, -1]]
-        self.assertEqual(expected, res)
+        a = np.array([[1, -12, 0],
+                      [4, 6, 20],
+                      [-1, 10, -1]])
+        tasks.step_right(a)
+        expected = np.array([[0, -12, 0],
+                             [0, 0, 0],
+                             [-1, 0, -1]])
+        self.assertEqual(expected.all(), a.all())
 
     def test_1f(self):
-        a = [[1, -12, 0],
-             [4, 6, 1],
-             [-1, 10, 1]]
-        res = tasks.lavrentev_buldaev(a)
-        expected = [[1, -12, 0],
-                    [4, 6, 1],
-                    [-1, 10, 1]]
-        self.assertEqual(expected, res)
+        a = np.array([[1, -12, 0],
+                      [4, 6, 1],
+                      [-1, 10, 1]])
+        tasks.step_right(a)
+        expected = np.array([[1, -12, 0],
+                             [4, 6, 1],
+                             [-1, 10, 1]])
+        self.assertEqual(expected.all(), a.all())
 
     def test_2t(self):
-        a = [[1, 1, -1],
-             [1, 20, -1],
-             [-1, 1, 1]]
-        res = tasks.lavrentev_buldaev(a)
-        expected = [[0, 0, -1],
-                    [0, 0, -1],
-                    [-1, 0, 0]]
-        self.assertEqual(expected, res)
+        a = np.array([[1, 1, -1],
+                      [1, 20, -1],
+                      [-1, 1, 1]])
+        tasks.step_right(a)
+        expected = np.array([[0, 0, -1],
+                             [0, 0, -1],
+                             [-1, 0, 0]])
+        self.assertEqual(expected.all(), a.all())
 
     def test_2f(self):
-        a = [[1, 1, -1],
-             [1, 20, 1],
-             [-1, 1, 1]]
-        res = tasks.lavrentev_buldaev(a)
-        expected = [[1, 1, -1],
-                    [1, 20, 1],
-                    [-1, 1, 1]]
-        self.assertEqual(expected, res)
+        a = np.array([[1, 1, -1],
+                      [1, 20, 1],
+                      [-1, 1, 1]])
+        tasks.step_right(a)
+        expected = np.array([[1, 1, -1],
+                             [1, 20, 1],
+                             [-1, 1, 1]])
+        self.assertEqual(expected.all(), a.all())
 
     def test_3(self):
-        a = [[1, 1, -1],
-             [1, 20, 0],
-             [-1, 1, 1]]
-        res = tasks.lavrentev_buldaev(a)
-        expected = [[0, 0, -1],
-                    [0, 0, 20],
-                    [-1, 0, 0]]
-        self.assertEqual(expected, res)
+        a = np.array([[1, 1, -1],
+                      [1, 20, 0],
+                      [-1, 1, 1]])
+        tasks.step_right(a)
+        expected = np.array([[0, 0, -1],
+                             [0, 0, 20],
+                             [-1, 0, 0]])
+        self.assertEqual(expected.all(), a.all())
 
     def test_3(self):
-        a = [[1, 1, -1],
-             [1, 20, -2],
-             [-1, 1, 1]]
-        res = tasks.lavrentev_buldaev(a)
-        expected = [[1, 1, -1],
-                    [1, 20, 21],
-                    [-1, 1, 1]]
-        self.assertEqual(expected, res)
+        a = np.array([[1, 1, -1],
+                      [1, 20, -2],
+                      [-1, 1, 1]])
+        tasks.step_right(a)
+        expected = np.array([[1, 1, -1],
+                             [1, 20, 21],
+                             [-1, 1, 1]])
+        self.assertEqual(expected.all(), a.all())
 
     def test_2_1(self):
-        a = [[1, 1, -1],
-             [1, 20, 21],
-             [-1, 1, 1]]
-        res = tasks.lavrentev(a)
+        a = np.array([[1, 1, -1],
+                      [1, 20, 21],
+                      [-1, 1, 1]])
+        res = tasks.can_not_step_right(a)
         expected = False
         self.assertEqual(expected, res)
 
     def test_2_2(self):
-        a = [[1, 1, -1],
-             [1, 20, 18],
-             [-1, 1, 1]]
-        res = tasks.lavrentev(a)
+        a = np.array([[1, 1, -1],
+                      [1, 20, 18],
+                      [-1, 1, 1]])
+        res = tasks.can_not_step_right(a)
         expected = False
         self.assertEqual(expected, res)
 
     def test_2_3(self):
-        a = [[1, 1, -1],
-             [1, 20, 19],
-             [-1, 1, 1]]
-        res = tasks.lavrentev(a)
+        a = np.array([[1, 1, -1],
+                      [1, 20, 19],
+                      [-1, 1, 1]])
+        res = tasks.can_not_step_right(a)
         expected = True
         self.assertEqual(expected, res)
 
