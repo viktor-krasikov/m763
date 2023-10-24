0aa192672c48d1bc6ee5550c4c59ca48c4ed9a84
Lavresss <st.la.2001@mail.ru>
2023-10-24 15:53:09 +0800

    убрал matplotlib

--- lesson3/oop-lavrentev/snake-oop.py
+++ lesson3/oop-lavrentev/snake-oop.py
@@ -2,17 +2,13 @@ import tasks
 import numpy as np
 import pygame
 import time
-from matplotlib import pyplot
 
 pygame.init()
 
 board = np.zeros((30, 40))
 disp = pygame.display.set_mode((400, 300))
-# board[15][15] = 1
-# board[15][16] = 2
-# board[20][10] = -2
-
 print(board.shape)
+
 
 def get_color(value):
     if value == -2:
@@ -29,8 +25,6 @@ def draw_board():
         for j, elem in enumerate(row):
             if elem != 0:
                 pygame.draw.rect(disp, get_color(elem), [j * 10, i * 10, 10, 10])
-    axim.set_data(board)
-    fig.canvas.flush_events()
     pygame.display.update()
 
 
@@ -41,12 +35,8 @@ for _ in range(4):
     tasks.create_horizontal_wall(board)
     tasks.create_vertical_wall(board)
 
-
 tasks.create_snake_if_need(board)
 tasks.create_food_if_need(board)
-pyplot.ion()
-fig,ax = pyplot.subplots()
-axim = pyplot.imshow(board)
 
 while not game_over:
     tasks.create_snake_if_need(board)
