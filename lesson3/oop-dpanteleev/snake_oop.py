153c95fcbb1b566aee853a6f88ffb6ff08d0b96b
Abara <pd2001pd@mail.ru>
2023-10-24 15:44:51 +0800

    Убрал matplotlib

--- lesson3/oop-dpanteleev/snake_oop.py
+++ lesson3/oop-dpanteleev/snake_oop.py
@@ -2,7 +2,6 @@ import pygame
 import numpy as np
 import time
 import tasks
-from matplotlib import pyplot
 
 pygame.init()
 board = np.zeros((30, 40))
@@ -28,19 +27,12 @@ def draw_board():
 
     pygame.display.update()
 
-    axim.set_data(board)
-    fig.canvas.flush_events()
-
 for i in range(4):
     tasks.create_horizontal_wall(board)
     tasks.create_vertical_wall(board)
 
 tasks.create_snake_if_need(board)
 tasks.create_food_if_need(board)
-
-pyplot.ion()
-fig, ax = pyplot.subplots()
-axim = pyplot.imshow(board)
 
 direction = 'R'
 game_over = False
