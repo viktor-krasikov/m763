cb907d0717b421704692ca4472c6b8641e433066
yugggcy <yugggcy@gmail.com>
2023-11-02 19:32:17 +0800

    Добавил кнопки для выбора действия
    Кнопка для проверки на палиндром и кнопка для вычисления суммы цифр

--- lesson7/MarbaevBot.py
+++ lesson7/MarbaevBot.py
@@ -8,17 +8,49 @@ def reverse(text):
 def is_palindrome(text):
     return text == reverse(text)
 
+def num_sum(text):
+    text = text.lower().replace(' ', '')
+    if text.isnumeric:
+        text = [int(i) for i in list(text)]
+        return sum(text)
+
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.from_user.id, "👋 Привет! Введи слово которое хочешь проверить на палиндром!")
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("Проверить на палиндром")
+    btn2 = types.KeyboardButton('Сумма цифр')
+    markup.add(btn1,btn2)
+    bot.send_message(message.from_user.id, "👋 Привет! Что хочешь сделать?", reply_markup=markup)
+
+palindrome = False
+summa = False
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    if (is_palindrome(message.text.lower().replace(' ',''))):
-        bot.send_message(message.from_user.id, "Да, это палиндром")
-    else:
-        bot.send_message(message.from_user.id, "Нет, это не палиндром")
+    global palindrome
+    global summa
+    if palindrome == True:
+
+        if (is_palindrome(message.text.lower().replace(' ',''))):
+            bot.send_message(message.from_user.id, "Да, это палиндром")
+        else:
+            bot.send_message(message.from_user.id, "Нет, это не палиндром")
+        palindrome = False
+
+    if summa == True:
+        bot.send_message(message.from_user.id, "Сумма цифр = " + str(num_sum(message.text)))
+        summa = False
+
+    if message.text == 'Проверить на палиндром':
+        palindrome = True
+        bot.send_message(message.from_user.id, "Введи слово")
+
+    if message.text == 'Сумма цифр':
+        summa = True
+        bot.send_message(message.from_user.id, "Введите число")
 
 
-bot.polling(none_stop=True, interval=0)
+
+
+bot.infinity_polling()
