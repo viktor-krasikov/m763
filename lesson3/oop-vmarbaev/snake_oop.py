e3a046f600869ed79bc422d4b6568472c190200b
Marbaev Vladislav <120071504+yugggcy@users.noreply.github.com>
2023-11-02 17:04:34 +0800

    Переделал змейку на ООП (#3)
    * Скопировал файлы для переделывания на ооп
    
    * Добавил в .gitignore папку pycache
    
    * Убрал matplotlib и лишнее из tasks
    
    * Переделывание на ООП, класс SnakeData
    
    * Поле board сделал private и небольшой рефакторинг
    
    * Рефакторинг поля direction
    
    * Скрыл лишние методы
    
    ---------
    
    Co-authored-by: Viktor Krasikov <viktor.krasikov@gmail.com>

---
+++ lesson3/oop-vmarbaev/snake_oop.py
@@ -0,0 +1,68 @@
+import pygame
+import time
+from tasks import SnakeData
+
+snake_data = SnakeData(30, 40)
+cell_size = 15
+pygame.init()
+
+disp = pygame.display.set_mode((snake_data.cols_count * cell_size, snake_data.rows_count * cell_size))
+
+
+def get_color(value):
+    if value == -2:
+        return (180, 0, 0)
+    elif value > 0:
+        return (200, 200, 0)
+    elif value == -1:
+        return (150, 150, 150)
+    return (0, 0, 100)
+
+
+def draw_board():
+    disp.fill((0, 0, 100))
+    for i in range(snake_data.rows_count):
+        for j in range(snake_data.cols_count):
+            elem = snake_data.get_elem(i, j)
+            if elem != 0:
+                pygame.draw.rect(disp, get_color(elem), [j * cell_size, i * cell_size, cell_size, cell_size])
+
+    pygame.display.update()
+
+
+for i in range(4):
+    snake_data.create_horizontal_wall()
+    snake_data.create_vertical_wall()
+
+snake_data.create_snake_if_need()
+snake_data.create_food_if_need()
+
+game_over = False
+while not game_over:
+    snake_data.create_food_if_need()
+    draw_board()
+
+    # Обработка событий
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+        if event.type == pygame.KEYDOWN:  # Добавить проверку на то, можно ли поворачивать
+            if event.key == pygame.K_LEFT:
+                snake_data.turn_left()
+            elif event.key == pygame.K_UP:
+                snake_data.turn_up()
+            elif event.key == pygame.K_RIGHT:
+                snake_data.turn_right()
+            elif event.key == pygame.K_DOWN:
+                snake_data.turn_down()
+            elif event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                quit()
+
+    snake_data.step()
+
+    draw_board()
+
+    time.sleep(0.1)
+
+pygame.quit()
